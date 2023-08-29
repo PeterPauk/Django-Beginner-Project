4506f707a9fbbdb258f25bda3768ab7272c84389
PeterPauk <petkopauk@gmail.com>
2023-08-29 11:02:19 +0200

    blog template prototype

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'blog/home.html')
+posts = [
+    {
+        'author' : 'PeterPauk',
+        'title' : 'The Industrial Revolution',
+        'content' : 'Kinda bad ngl',
+        'date_posted' : '29. August 2023'
+    },
+
+    {
+        'author' : 'TheDarkOne',
+        'title' : 'Masonic Rituals II',
+        'content' : 'lets get into it',
+        'date_posted' : '17. June 1987'
+    },
+
+    {
+        'author' : 'Emonic',
+        'title' : 'Esoteric Literature',
+        'content' : 'we are going to pray tonight',
+        'date_posted' : '31. September 2025'
+    }
+]
+
+def home(request,):
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html',{'title':'About'})
 
